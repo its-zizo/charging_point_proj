e64d91b7d6274738f8af2ad424cad57468f3d3b4
Ziyad Nabhan <zozo2005.nbhan@gmail.com>
2025-06-27 08:45:49 +0300

    main.py modification

--- main.py
+++ main.py
@@ -2,42 +2,46 @@ import json
 import datetime
 import os
 
-# def File_Generator ():
-"""
-    this function create new json file every day
-"""
-#     todayS = str (datetime.date.today())
-#     fileName = f'data/clients_{todayS}.json'
-#     return fileName
+print ('Welcome To Charging Point Management System!')
 
-# def import_data():
-"""
-    this function import json file data
-"""
-#     fileName = File_Generator()
-#     if not os.path.exists(fileName):
-#         with open(fileName, 'w') as file:
-#             json.dump({},file , indent=4)
-#     with open (fileName,'r') as file:
-#         return json.load(file)
+def File_Generator ():
+    """
+    description : This function create new json file every day.
+    """
+    todayS = str (datetime.date.today())
+    fileName = f'data/clients_{todayS}.json'
+    return fileName
+
+def import_data():
+    """
+    description : This function import json file data.
+    """
+    fileName = File_Generator()
+    if not os.path.exists(fileName):
+         with open(fileName, 'w') as file:
+             json.dump({},file , indent=4)
+    with open (fileName,'r') as file:
+         return json.load(file)
 
 #I'll complete this function ASAP.
+    #Done
 
- # def save_data(data):
-"""
-    this function save client data in json file
-"""
- #     fileName = File_Generator():
- #     with open(fileName , 'w') as file:
- #         json.dump()
+def save_data(data):
+    """
+    description : This function save client's data in json file.
+    """
+    fileName = File_Generator()
+    with open(fileName , 'w') as file:
+        json.dump(data ,file , indent=4 )
 
 
-def Add_New_Client():
-    #Test - I'll Add them later to another function 
-    name = input ("Please Enter client's Name: ")
-    device = []
-    price = 0
-
+def Add_New_Client(name , device , price):
+    """
+    input : name: client's Name
+    select: device
+    output : client's ID , client's Name , devices , total Price
+    """
+    
     while True:
         print("1.Smart Phone")
         print("2.Small Battery")
@@ -56,7 +60,7 @@ def Add_New_Client():
         elif selection == '1' or selection == '2' or selection == '3':
             
                     while True:
-                        print("is your device with charger?")
+                        print("\nis your device with charger?")
                         print('1.Yes')
                         print('2.No')
                         ans =  input('Please type a number: ') 
@@ -84,22 +88,68 @@ def Add_New_Client():
                             break
                                 
                         else:
-                            print ('Please Select 1 or 2!')
+                            print ('\nPlease Select 1 or 2!')
 
                     break
                 
         else:
-            print("Please Type a 1-5 number!")
-        #Next step in this func: do you want to add another device?
-#function Test
-    print (name)
-    print (device)
-    print (price)
-    userInfo = {
-        'clientName': name ,
-        "devices":device,
-        'price':price
-    }
-    print(userInfo)
+            print("\nPlease Type a number between 1-5!")
 
-print (Add_New_Client())
+    while True:
+
+        print("\n1.Yes")
+        print("2.No")
+        answer = input('Do You want to add another device? ')
+        if answer == '1':
+            Add_New_Client(name , device , price)
+        elif answer == '2':
+            clientInfo = {
+                'client name' : name,
+                'devices' : device,
+                'price' : price
+                }
+            print (f'name: {name}')
+            print ('devices:', ','.join(device))
+            print (f'Total Price: {price}')
+
+            while True:
+                print("\n1.Yes")
+                print("2.No")
+                ans = input (f'Are you sure that you want to add the client {name}?')
+                if ans == '1':
+                    data = import_data()
+                    id = len(data) +1
+                    data[id] = clientInfo
+                    save_data(data)
+                    print (f'\n{name} Added Successfully!')
+                    print (f"{name}'s ID : {id}")
+                    anyKey = input ("\nPlease press any key to continue. ")
+                    main()
+                elif ans == "2":
+                    main()
+                else:
+                    print ("\nPlease type 1 or 2!")
+            break
+        else:
+            print ('\nYou can only select 1 to add new device or 2 to go to the next screen!')
+
+   
+ #Next function : profit Tracker for 'Profit' Section.
+
+
+def main():
+    print ("1.Add New Client")
+    print ("2.ID Search")
+    print ("3.Profit")
+    print ("4.Exit")
+    
+    choice = input("\nPlease select an option: ").strip()
+    if choice == '1':
+        print ('\n---Add New Client---')
+        name = input ("Please Enter client's Name: ").strip()
+        device = []
+        price = 0
+        Add_New_Client(name, device , price)
+
+
+main()
